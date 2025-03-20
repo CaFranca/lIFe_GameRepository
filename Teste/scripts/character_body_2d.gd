d8441d01ec19fb6532e9dd19b0ccb68a2bc03e94
Caique Franca <caique.franca@aluno.ifsp.edu.br>
2025-03-20 16:25:15 -0300

    Movimentacao de camera

--- Teste/scripts/character_body_2d.gd
+++ Teste/scripts/character_body_2d.gd
@@ -10,24 +10,31 @@ func _physics_process(delta: float) -> void:
 	# Movimentação livre no eixo Y (vertical) e X (horizontal)
 	var direction_x := Input.get_axis("ui_left", "ui_right")  # Movimento horizontal
 	var direction_y := Input.get_axis("ui_up", "ui_down")  # Movimento vertical
-
+	var jump= Input.is_action_just_pressed("ui_accept")
+	if jump:
+		animation.play("Jump")
+		print("pulou")
 	# Movimentação horizontal (esquerda/direita)
 	if direction_x != 0:
 		velocity.x = direction_x * SPEED  # Aplica a velocidade de movimento horizontal
 		# Animações de movimento horizontal
 		if direction_x < 0:
 			$AnimatedSprite2D.flip_h = true  # Vira o sprite para a esquerda
+			print("Andou para esquerda")
 			animation.play("Run")  # Animação de correr para esquerda
 		else:
 			$AnimatedSprite2D.flip_h = false  # Vira o sprite para a direita
+			print("Andou para direita")
 			animation.play("Run")  # Animação de correr para direita
 	# Movimentação vertical (cima/baixo)
 	elif direction_y != 0:
 		velocity.y = direction_y * SPEED  # Aplica a velocidade de movimento vertical
 		# Animações de movimento vertical
 		if direction_y < 0:
+			print("Andou para cima")
 			animation.play("Run_Up")  # Animação de correr para cima
 		else:
+			print("Andou para baixo")
 			animation.play("Run_Down")  # Animação de correr para baixo
 	else:
 		# Quando não há movimento, desacelera suavemente até parar
@@ -35,8 +42,4 @@ func _physics_process(delta: float) -> void:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 		animation.play("Idle")  # Animação de ficar parado
 
-	# Aplicar a física de movimento (horizontal + gravidade no eixo Y)
-	# Como o personagem vai se mover apenas em 2D, a gravidade é aplicada apenas no eixo Y
-	
-
-	move_and_slide()  # O vetor UP garante que a gravidade seja aplicada corretamente
+	move_and_slide()
